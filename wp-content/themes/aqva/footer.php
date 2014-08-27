11a19d201c134763e913eb7678cb5cae8b97b0d8
Thai Nguyen <thai@bliss-interactive.net>
2014-08-27 22:10:43 +0700

    .

--- wp-content/themes/aqva/footer.php
+++ wp-content/themes/aqva/footer.php
@@ -48,14 +48,14 @@ var addthis_product = 'wpp-265';
 	jQuery('html').addClass('js');	
 	
 	//=================================== MENU ===================================//
-	jQuery("ul.sf-menu").supersubs({ 
-	minWidth		: 10,		// requires em unit.
+/*	jQuery("ul.sf-menu").supersubs({ 
+	minWidth		: 13,		// requires em unit.
 	maxWidth		: 13,		// requires em unit.
 	extraWidth		: 3	// extra width can ensure lines don't sometimes turn over due to slight browser differences in how they round-off values
 						   // due to slight rounding differences and font-family 
 	}).superfish();  // call supersubs first, then superfish, so that subs are 
 					 // not display:none when measuring. Call before initialising 
-					 // containing tabs for same reason. 
+					 // containing tabs for same reason. */
 	
 	//=================================== MOBILE MENU DROPDOWN ===================================//
 	jQuery('#topnav').tinyNav({
@@ -78,24 +78,7 @@ var addthis_product = 'wpp-265';
 		});
 	}
 	</script>
-		<script type="text/javascript">
-	jQuery(document).ready(function(){
-		//=================================== TABS AND TOGGLE ===================================//
-		//jQuery tab
-		jQuery(".tab-content").hide(); //Hide all content
-		jQuery("ul.tabs li:first").addClass("active").show(); //Activate first tab
-		jQuery(".tab-content:first").show(); //Show first tab content
-		//On Click Event
-		jQuery("ul.tabs li").click(function() {
-			jQuery("ul.tabs li").removeClass("active"); //Remove any "active" class
-			jQuery(this).addClass("active"); //Add "active" class to selected tab
-			jQuery(".tab-content").hide(); //Hide all tab content
-			var activeTab = jQuery(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
-			jQuery(activeTab).show(); //Fade in the active content
-			return false;
-		});
-	});
-	</script>
+
 	<!--wp_footer-->
     <?php wp_footer(); ?>
     </body>
